1d9bbe3bc6c7b06f1264ec8bcc72592c37e9e450
Juliane Pires <juliane.pires@ltm.digital>
2023-05-25 10:05:50 -0300

    Create function to manipulate data from csv

--- app.py
+++ app.py
@@ -10,3 +10,10 @@ st.title("Motor Vehicle Collisions in New York City")
 st.markdown("This application is a Streamlit dashboard that can be used "
             "to analyze motor vehicle collisions in NYC 🗽💥🚗")
 
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
+    data.dropna(subset=['LATITUDE', 'LONGITUDE'], implace=True)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data.rename(columns={'crash_date_crash_time': 'date/time'}, implace=True)
+    return data
